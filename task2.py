991a91daaf6a2a453403552b645346e82c6de804
PetelinD <danilapetelin@gmail.com>
2024-11-08 18:59:30 +0300

    hw7

--- task2.py
+++ task2.py
@@ -10,3 +10,21 @@
 # если недостаточно, выведите сообщение "Недостаточно средств".
 
 # Метод get_balance, который возвращает текущий баланс.
+
+class BankAccount:
+    def __init(self):
+        self.balance = 0
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance>= amount:
+            self.balance -=amount
+            return 1
+        else:
+            print("Недостаточно средств")
+            return -1
--- task3.py
+++ task3.py
@@ -6,3 +6,15 @@
 # в список.
 
 # Метод average_grade, который возвращает средний балл студента.
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.grades = []
+
+    def add_grade(self,grade):
+        self.grades.append(grade)
+
+    def average_grade(self):
+        return sum(self.grades)/len(self.grades) if len(self.grades) != 0 else None
